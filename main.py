aa29dd750339332080fc782e13aef236a4bb8bc9
zerpkelly-debug <zerpkelly@gmail.com>
2026-05-14 17:37:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 ATOK_FILE_NAME = "accounts.txt"
 PROXY_FILE_NAME = "proxies.txt"
 
-CONCURRENT_ACCOUNTS = 5   # একসাথে ২০টি একাউন্ট চলবে
-BOTS_PER_ACCOUNT = 5      # প্রতি একাউন্টে ২০টি বট থ্রেড
+CONCURRENT_ACCOUNTS = 20   # একসাথে ২০টি একাউন্ট চলবে
+BOTS_PER_ACCOUNT = 20      # প্রতি একাউন্টে ২০টি বট থ্রেড
 BASE_LIMIT = 870          # প্রতিটি একাউন্টের টার্গেট
 
 # লাইভ লগিং ফাংশন (গিটহাবের জন্য অপ্টিমাইজড)
